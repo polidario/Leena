cfec0b12a0f31fca8e67f10e9a2709b9f6dc6ff4
bbbpppolly <71621723+bbbpppolly@users.noreply.github.com>
2021-05-15 10:10:45 +0800

    New Lesson: Product AJAX API
    In this change, we learned how to get the products using AJAX Product API

--- assets/application.js
+++ assets/application.js
@@ -52,3 +52,31 @@ if(localeItems.length > 0) {
         });
     });
 }
+
+var productInfoAnchors = document.querySelectorAll("#productInfoAnchor");
+
+var productModal = new bootstrap.Modal(document.getElementById('productInfoModal'), {});
+
+if(productInfoAnchors.length > 0) {
+    productInfoAnchors.forEach(item => {
+        item.addEventListener("click", event => {
+            
+            var url = '/products/' + item.getAttribute('product-handle') + '.js';
+
+            fetch(url)
+            .then((resp) => resp.json())
+            .then(function(data) {
+                console.log(data);
+
+                document.getElementById("productInfoImg").src = data.images[0];
+                document.getElementById("productInfoTitle").innerHTML = data.title;
+                document.getElementById("productInfoPrice").innerHTML = item.getAttribute('product-price');
+                document.getElementById("productInfoDescription").innerHTML = data.description;
+
+                productModal.show();
+            });
+
+            
+        });
+    });
+}
